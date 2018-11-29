3ce517dd32639b60f93fff9b1c2352a0ff5a839b
hasinibujji <42571653+hasinibujji@users.noreply.github.com>
2018-11-29 12:26:22 +0530

    Update 80.py

--- 80.py
+++ 80.py
@@ -1,9 +1,9 @@
 number=int(raw_input(""))
 digits = []
 while number > 0:
-    r = number % 10
-    if r & 1 != 0:
-        digits.append(str(r))
+    s = number % 10
+    if s & 1 != 0:
+        digits.append(str(s))
     number=number/10
 digits = reversed(digits)
 print (" ".join(digits))
