7dd69f14773d731b41a241aee8f3d95c10ec795d
hasinibujji <42571653+hasinibujji@users.noreply.github.com>
2018-12-13 11:29:16 +0530

    Update pl9.py

--- pl9.py
+++ pl9.py
@@ -1,9 +1,9 @@
 lower,upper=map(int,raw_input().split())
 count=0
-for num in range(lower,upper+1):
-    if num>1:
-        for x in range(2,num):
-            if(num%x==0):
+for number in range(lower,upper+1):
+    if number>1:
+        for x in range(2,number):
+            if(number%x==0):
                 break
         else:
           count=count+1
