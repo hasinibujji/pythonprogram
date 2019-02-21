fd376ce922b8f2f36fbf581eb4035025b6be4bfb
hasinibujji <42571653+hasinibujji@users.noreply.github.com>
2019-02-21 11:11:28 +0530

    Update begginer92.py

--- begginer92.py
+++ begginer92.py
@@ -1,3 +1,6 @@
-m1=int(raw_input())
-li=[int(a) for a in raw_input().split()]
-print sum(li)
+m1,n1,k1 = [int(a) for a in raw_input().split()]
+sum=0
+for i in range(k1):
+ sum+=m1+(i*n1)
+print (sum)
+
