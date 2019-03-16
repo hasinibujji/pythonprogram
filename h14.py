9d0d400bcc9e0dd2651216763529b059a13b61a9
hasinibujji <42571653+hasinibujji@users.noreply.github.com>
2019-03-16 12:26:16 +0530

    Update h14.py

--- h14.py
+++ h14.py
@@ -1,12 +1,12 @@
-def permutation(lst): 
-    if len(lst) == 0: 
+def permutation(lst1): 
+    if len(lst1) == 0: 
         return []
-    if len(lst) == 1: 
-        return [lst]
+    if len(lst1) == 1: 
+        return [lst1]
     l = [] 
-    for i in range(len(lst)): 
-       m = lst[i] 
-       remLst = lst[:i] + lst[i+1:] 
+    for i in range(len(lst1)): 
+       m = lst1[i] 
+       remLst = lst1[:i] + lst1[i+1:] 
        for p in permutation(remLst): 
            l.append([m] + p) 
     return l
